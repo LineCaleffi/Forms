6f916af0e8799c6df09e07c6d20f0f0137d6b7f5
aline.caleffi@nava.com.br <alinecaleffi97@gmail.com>
2023-02-13 11:41:42 -0300

    Verificando dados do Form no template com JSON

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TemplateFormComponent } from './template-form/template-form.component';
 import { DataFormComponent } from './data-form/data-form.component';
+import { FormDebugComponent } from './form-debug/form-debug.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataFormComponent
+    DataFormComponent,
   ],
   imports: [
     BrowserModule,
